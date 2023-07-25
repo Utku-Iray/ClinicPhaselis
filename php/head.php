02d9261ca4e14133e9957344e3cc5696ae9d2b33
Utku-Iray <51332027+Utku-Iray@users.noreply.github.com>
2023-07-25 17:11:40 +0300

    rev

--- php/head.php
+++ php/head.php
@@ -1,37 +1,38 @@
 <head>
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <!-- Bootstrap CSS --> 
-        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-        <!-- Animate CSS --> 
-        <link rel="stylesheet" href="assets/css/animate.min.css">
-        <!-- Meanmenu CSS -->
-        <link rel="stylesheet" href="assets/css/meanmenu.css">
-        <!-- Boxicons CSS -->
-        <link rel="stylesheet" href="assets/css/boxicons.min.css">
-        <!-- Flaticon Two CSS -->
-        <link rel="stylesheet" href="assets/css/flaticon-two.css">
-        <!-- Flaticon CSS -->
-        <link rel="stylesheet" href="assets/css/flaticon.css">
-        <!-- Odometer CSS -->
-        <link rel="stylesheet" href="assets/css/odometer.min.css">
-        <!-- Nice Select CSS -->
-        <link rel="stylesheet" href="assets/css/nice-select.min.css">
-        <!-- Carousel CSS -->
-        <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
-        <!-- Carousel Default CSS -->
-        <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
-        <!-- Popup CSS -->
-        <link rel="stylesheet" href="assets/css/magnific-popup.min.css">
-        <!-- Style CSS -->
-        <link rel="stylesheet" href="assets/css/style.css">
-        <!-- Dark CSS -->
-        <link rel="stylesheet" href="assets/css/dark.css">
-        <!-- Responsive CSS -->
-		<link rel="stylesheet" href="assets/css/responsive.css">
-		
-		<title>Clinic Phaselis</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <!-- Animate CSS -->
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <!-- Meanmenu CSS -->
+    <link rel="stylesheet" href="assets/css/meanmenu.css">
+    <!-- Boxicons CSS -->
+    <link rel="stylesheet" href="assets/css/boxicons.min.css">
+    <!-- Flaticon Two CSS -->
+    <link rel="stylesheet" href="assets/css/flaticon-two.css">
+    <!-- Flaticon CSS -->
+    <link rel="stylesheet" href="assets/css/flaticon.css">
+    <!-- Odometer CSS -->
+    <link rel="stylesheet" href="assets/css/odometer.min.css">
+    <!-- Nice Select CSS -->
+    <link rel="stylesheet" href="assets/css/nice-select.min.css">
+    <!-- Carousel CSS -->
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <!-- Carousel Default CSS -->
+    <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+    <!-- Popup CSS -->
+    <link rel="stylesheet" href="assets/css/magnific-popup.min.css">
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- Dark CSS -->
+    <link rel="stylesheet" href="assets/css/dark.css">
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" href="assets/css/responsive.css">
 
-        <link rel="icon" type="image/png" href="assets/images/logo/fav-icon.png">
-    </head>
+    <title>Clinic Phaselis</title>
+    <meta name="description" content="Clinic Phaselis is a medical tourism and medical consulting company that offers quality and excellence in aesthetic surgery with reliability and safety.">
+    <meta name="keywords" content="Plastic Sruger Body, Hair Transplant, Bariatric Surgery, Dental Procedures, Plastic Surgery Breast, Plastic Surgery Face, Plastic Surgery Procedures, ">
+    <link rel="icon" type="image/png" href="assets/images/logo/fav-icon.png">
+</head>
